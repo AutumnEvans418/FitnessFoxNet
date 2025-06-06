b847062cc9290589908e7ec805fe020c0c1024f2
Autumn <AutumnFox418@gmail.com>
2025-06-06 01:02:17 -0500

    cleanup

--- FitnessFox/Data/UserGoal.cs
+++ FitnessFox/Data/UserGoal.cs
@@ -1,5 +1,20 @@
 namespace FitnessFox.Data
 {
+    public enum UserGoalType
+    {
+        Weight,
+        WeightLoss_Week,
+        Bmi,
+        Calories,
+        Cholesterol,
+        Sodium,
+        VitaminK,
+        Sugar,
+        WorkoutMinutes_Week,
+        LbsLifted_Week,
+        CaloriesBurned_Week,
+    }
+
     public class UserGoal
     {
         public int Id { get; set; }
