4405477c8ba044e74ee55e9c2b1255926c2acd9b
Autumn <AutumnFox418@gmail.com>
2025-08-03 18:11:22 -0500

    removed notes

--- FitnessFox.Components/Data/Vitals/UserVital.cs
+++ FitnessFox.Components/Data/Vitals/UserVital.cs
@@ -7,7 +7,6 @@ namespace FitnessFox.Data.Vitals
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public UserVitalType Type { get; set; }
         public float Value { get; set; }
-        public string? Notes { get; set; }
         public ApplicationUser User { get; set; } = null!;
     }
 }
