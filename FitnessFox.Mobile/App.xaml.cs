3717bccbdd6d1ae8dd5bee137823039abd5fd9ed
Autumn <AutumnFox418@gmail.com>
2025-08-03 11:13:15 -0500

    added mobile

---
+++ FitnessFox.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessFox.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FitnessFox.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FitnessFox.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
